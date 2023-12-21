7e47f72dc756354ffeb5c054389276b268a95a27
Katya <katyavillano@gmail.com>
2023-12-21 12:58:06 -0500

    solution finished

--- problems/zigZagConversion.js
+++ problems/zigZagConversion.js
@@ -4,13 +4,12 @@
  * @return {string}
  */
 const convert = (s, numRows) => {
+    if (s.length <= numRows) return s;
     // at first its numRows - 1 * 2 its a corner 
     // then numRows - 2 * 2 and 1 * 2
     // then numRows - 3 * 2 and 2 * 2
     // then numRows - 4 * 2 and 3 * 2
-
     let result = '';
-    let fullColumns = Math.ceil(s.length / ((numRows * 2) -2));
 
     // base could be when new string matches input length
     // other base case to increment j  it when youve gotten to the end 
@@ -19,18 +18,19 @@ const convert = (s, numRows) => {
     // s.length / numRows * 2 - 2 to ceiling
     // 14 / 6 ---> 3
     const recurse = (i) => {
-        if (i === fullColumns) return;
+
+        if (i === numRows) return;
         let j = i;
 
         while(j < s.length){
             // add it 
-            
             // if were not at the bottom row
             if (i !== numRows - 1){
               result += s[j];
               j += (numRows - (i + 1)) * 2;
             }
-            // of were not at the top row
+
+            // if were not at the top row
             if (i !== 0){
                 if (j < s.length){
                 result += s[j];
@@ -45,4 +45,4 @@ const convert = (s, numRows) => {
     return result;
 };
 
-console.log(convert("PAYPALISHIRING", 4))
+console.log(convert("PAYPALISHIRING", 2))
